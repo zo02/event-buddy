f04627b30712686bf6bd0dea05e48bfc0d77b415
Zofia Borys <zosiaborys.zb@gmail.com>
2024-09-27 14:49:38 +0200

    authentication implemented

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
 # Create your models here.
+
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a simple example of a React component.</p>
+      </div>
+    );
+}
+
+export default Home;
