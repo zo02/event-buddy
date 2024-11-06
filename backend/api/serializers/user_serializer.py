5624fbb3bd943b62f3d7a64e81d0f68c2adec22a
Zofia Borys <zosiaborys.zb@gmail.com>
2024-11-06 20:59:58 +0100

    Fixed User Serializer

--- backend/api/serializers/user_serializer.py
+++ backend/api/serializers/user_serializer.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'email', 'status', 'interests', 'avatar']
+        fields = ['id', 'username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
